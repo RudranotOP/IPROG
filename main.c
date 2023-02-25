577fbdd67f4135e6121155e8fbec08d3b1d94864
RudranotOP <126277385+RudranotOP@users.noreply.github.com>
2023-02-25 11:48:48 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-char team1[50],team2[50],t_won[50],t_choose[50];
+char team1[50],team2[50],t_won[50],t_choose[50],player[50];
 void member_input()
 {
-    printf("Helo");
-
+    FILE *fp, *fp1;
+    fp=fopen(team1,"w");
+      if (fp == NULL)
+    {
+        printf("Error opening file!\n");
+        //return 1;
+    }
+    
+    for (int i = 1; i <= 11; i++) 
+    {
+        printf("enter player %d",i);
+        scanf("%s",player);
+        fprintf(fp, "%s\n", player);
+    }
+    
+    
+    fclose(fp);
+    fp1=fopen(team2,"w");
+      if (fp1 == NULL)
+    {
+        printf("Error opening file!\n");
+        //return 1;
+    }
+    
+    for (int j= 1; j <= 11; j++) 
+    {
+        printf("enter player %d",j);
+        scanf("%s",player);
+        fprintf(fp1, "%s\n", player);
+    }
+    
+    
+    fclose(fp1);
 }
+
 void input()
 {
     
