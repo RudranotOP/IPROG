e20c203ec80c987604c35bdd27c19ed1cd107e42
RudranotOP <126277385+RudranotOP@users.noreply.github.com>
2023-03-07 09:53:21 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
 
-char team1[50],team2[50],t_won[50],t_choose[50],player[50];
+
+char team1[50],team2[50],t_won[50],t_choose[50],player[50],striker[50],nonstriker[50],bowler[50];
+int overs,run,score=0;
+
+typedef struct batsman{
+  char name[50];
+  int balls_faced;
+  int runs;
+  float strikerate;
+} ba;
+
+typedef struct bowler{
+  char name[50];
+  int runs_given;
+  int wikets_taken;
+  float economy;
+} bow;
+
 void member_input()
 {
     FILE *fp, *fp1;
@@ -11,10 +29,11 @@ void member_input()
         printf("Error opening file!\n");
         //return 1;
     }
-    
+
+    printf("For %s\n",team1);   
     for (int i = 1; i <= 11; i++) 
     {
-        printf("enter player %d",i);
+        printf("enter player %d: ",i,team1);
         scanf("%s",player);
         fprintf(fp, "%s\n", player);
     }
@@ -28,9 +47,10 @@ void member_input()
         //return 1;
     }
     
+    printf("For %s\n",team2);  
     for (int j= 1; j <= 11; j++) 
     {
-        printf("enter player %d",j);
+        printf("enter player %d: ",j,team2);
         scanf("%s",player);
         fprintf(fp1, "%s\n", player);
     }
@@ -39,10 +59,38 @@ void member_input()
     fclose(fp1);
 }
 
+void match_input(){
+    int c,d,wicket;
+    float runrate;
+    printf("Enter Batsman1:\n");
+    scanf("%s",striker);
+    printf("Enter Batsman2:\n");
+    scanf("%s",nonstriker);
+    printf("Enter Bowler:\n");
+    scanf("%s",bowler);
+    for (int i = 1; i <=overs; i++)
+    {
+        printf("For %dth over",i);
+        for (int j = 1; j <=6; j++)
+        {
+            printf("\nEnter the runs scored in %dth ball: ",j);
+            scanf("%d",&run);
+            if (run>6)
+            {
+                printf("Enter Valid Run\n");
+                j--;
+            }
+            score+=run;
+        }
+        
+    }
+    printf("The score is %d",score);
+}
+
 void input()
 {
     
-    int overs;
+
     printf("Enter names of two teams\n");
     scanf("%s",team1);
     scanf("%s",team2);
@@ -58,5 +106,12 @@ void input()
 
 int main()
 {
+   ba bt[11];
+   bow bo[11];
    input();
+   printf("For first innings\n");
+   match_input();
+   printf("For second innings\n");
+   match_input();
+   return 0;
 }    
